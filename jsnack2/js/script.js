80062096c370912af0354acf117a9300f96a643a
cibs84 <d.tilesi@gmail.com>
2022-06-10 10:02:00 +0200

    jsnack2 - simplified

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,14 +1,16 @@
 // CHIEDO ALL'UTENTE DI INSERIRE UN NUMERO A 4 CIFRE
-let myString = prompt('Inserire un numero con 4 cifre');
-console.log(myString);
+let userNumber = prompt('Inserire un numero con 4 cifre');
+
 // CREO VARIABILE PER LA SOMMA
-let somma = 0;
-// SCORRO LE SINGOLE CIFRE DEL NUMERO INSERITO DALL'UTENTE VARIABILE CREATA PER LA SOMMA
-for (let i = 0; i < myString.length; i++) {
-    const number = parseInt(myString[i]);
-    // SOMMO LE SINGOLE CIFRE DEL NUMERO INSERITO DALL'UTENTE NELLA VARIABILE CREATA PER LA SOMMA
-    somma += number;
+let sum = 0;
+
+// SCORRO LE SINGOLE CIFRE DEL NUMERO INSERITO DALL'UTENTE
+for (let i = 0; i < userNumber.length; i++) {
+    const thisNumber = parseInt(userNumber[i]);
+    
+    // SOMMO LE SINGOLE CIFRE DEL NUMERO INSERITO DALL'UTENTE
+    sum += thisNumber;
 }
 
 // STAMPO IN CONSOLE LA SOMMA
-console.log(somma);
+console.log(sum);
