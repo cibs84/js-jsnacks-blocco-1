7d82b55df44f3fe2769f1c840c481f4e35264569
cibs84 <d.tilesi@gmail.com>
2022-06-10 10:28:05 +0200

    jsnack4 - simplified

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -3,11 +3,13 @@
 // se è dispari inseriscilo nell’array.
 
 // CREO UN ARRAY DOVE INSERIRE I NUMERI DISPARI IMMESSI DALL'UTENTE
-let oddNumbers = [];
+const oddNumbers = [];
 
-// CREO CICLO FOR DOVE CHIEDERE ALL'UTENTE DI IMMETTERE PER 6 VOLTE UN NUMERO
+// PER 6 VOLTE
 for (i = 0; i < 6; i++) {
-    let userNumber = parseInt( prompt(`${i + 1}/6 - Inserisci un numero`) );
+    // CHIEDO ALL'UTENTE DI INSERIRE UN NUMERO
+    const userNumber = parseInt( prompt(`${i + 1}/6 - Inserisci un numero`) );
+
     // SE IL NUMERO IMMESSO E' DISPARI ALLORA LO INSERISCO NELL'ARRAY PRECEDENTEMENTE CREATO 'oddNumbers'
     if (userNumber % 2 !== 0) {
         oddNumbers.push(userNumber);
