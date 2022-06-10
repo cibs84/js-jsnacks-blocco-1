b8db1aef51e2b352c32f582948317202e24292e1
cibs84 <d.tilesi@gmail.com>
2022-06-10 09:49:28 +0200

    jsnack1 - simplified

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -5,25 +5,18 @@
 
 // CREO ARRAY
 const numbers = [3, 6, 93, 67, 89, 24, 68, 71, 68];
-let numbersOdd = [];
-let numbersEven = [];
 
 // SCORRO I NUMERI E VERIFICO CON % I MULTIPLI DI 2
 for (let i = 0; i < numbers.length; i++) {
     let thisNumber = numbers[i];
-    // SE IL NUMERO E' MULTIPLO DI 2 LO INSERISCO NELL'ARRAY numbersEven
+    // SE IL NUMERO E' PARI SARA' STAMPATO NELL'ELEMENTO HTML CON ID 'even'
+    let divId;
     if (thisNumber % 2 === 0) {
-        numbersEven.push(thisNumber);
-    // ALTRIMENTI IL NUMERO VERRA' INSERITO NELL'ARRAY numbersOdd
+        divId = 'even';
+    // ALTRIMENTI IL NUMERO SARA' STAMPATO NELL'ELEMENTO HTML CON ID 'odd'
     } else {
-        numbersOdd.push(thisNumber);
+        divId = 'odd';
     }
+
+    document.getElementById(divId).innerHTML += thisNumber;
 }
-
-// SELETTORI ELEMENTI HTML DOVE STAMPARE I DUE ARRAY PARI/DISPARI
-const evenElement = document.getElementById('even');
-const oddElement = document.getElementById('odd');
-
-// STAMPO IN HTML I DUE ARRAAY PARI/DISPARI
-evenElement.innerHTML = numbersEven;
-oddElement.innerHTML = numbersOdd;
