19bf4617f94e9ddc0aaadf03117b7668feacd796
cibs84 <d.tilesi@gmail.com>
2022-06-10 10:11:09 +0200

    jsnack3 - simplified

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,12 +1,16 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-// CREO COSTANTE PER FARE LA SOMMA DEI NUMERI IMMESSI DALL'UTENTE
+// CREO UNA COSTANTE PER FARE LA SOMMA DEI NUMERI IMMESSI DALL'UTENTE
 let sum = 0;
 
-// CON LOOP FOR CHIEDO PER 10 VOLTE ALL'UTENTE DI INSERIRE UN NUMERO CHE ANDRO' POI A SOMMARE
+// PER 10 VOLTE
 for (i = 0; i < 10; i++) {
-    sum += parseInt( prompt(`${i + 1}/10 - Inserisci un numero`) );
+    // CHIEDO DI INSERIRE UN NUMERO
+    const userName = parseInt( prompt(`${i + 1}/10 - Inserisci un numero`) );
+    
+    // OGNI NUMERO LO AGGIUNGO ALLA SOMMA PRECEDENTE
+    sum += userName;
 }
 
 // STAMPO IN HTML LA SOMMA DEI NUMERI IMMESSI
